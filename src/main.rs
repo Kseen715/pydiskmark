e96902561249ada2e1f6f7b79288c3544188c6eb
Kseen715 <korenevdn@gmail.com>
2025-07-28 23:41:41 +0300

    Update dependencies and enhance FIO job handling; add logging options and improve configuration parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use clap::Parser;
+
+use serde_json;
+use indicatif;
 mod logger;
+use log;
+static INIT: std::sync::Once = std::sync::Once::new();
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// The path to the file to test
     #[arg(short, long, default_value = "none")]
-    path: String,
+    path: Option<String>,
+    /// Log level for the application (e.g., trace, debug, info, warn, error)
+    #[arg(long, default_value = "info")]
+    log_level: Option<String>,
 }
 
 mod fio {
+    use indicatif::{ ProgressBar, ProgressStyle };
+
     #[allow(dead_code)]
     pub fn is_fio_available() -> bool {
         use std::process::Command;
@@ -33,6 +43,7 @@ mod fio {
         String::from_utf8_lossy(&output.stdout).trim().to_string().replace("fio-", "")
     }
 
+    #[allow(dead_code)]
     pub fn read_config_file(path: &str) -> configparser::ini::Ini {
         use std::fs;
         use std::io::Read;
@@ -45,12 +56,19 @@ mod fio {
         config
     }
 
+    pub fn read_config_str(config: &str) -> configparser::ini::Ini {
+        use configparser::ini::Ini;
+        let mut ini = Ini::new();
+        ini.read(config.to_string()).expect("Failed to parse INI file");
+        ini
+    }
+
     /// Generates FIO job configurations from a given INI configuration.
     /// It parallelizes the sections into the separate job configurations.
     pub fn gen_fio_job_configs(config: &configparser::ini::Ini) -> Vec<configparser::ini::Ini> {
         let map = config.get_map().unwrap();
         let sections = config.sections();
-        println!("{:?}", sections);
+        // println!("{:?}", sections);
         let mut job_configs = Vec::new();
         // iterate over sections
         for (section_name, _) in map.iter() {
@@ -64,13 +82,26 @@ mod fio {
                 }
                 _ = current_job.remove_section(key);
             }
+            if section_name != "global" {
+                current_job.set(&section_name, "startdelay", Some(std::string::String::from("0")));
+            }
             job_configs.push(current_job);
         }
         job_configs
     }
 
-    pub fn run_jobs(jobs: Vec<configparser::ini::Ini>) {
+    pub fn run_jobs(jobs: Vec<configparser::ini::Ini>) -> Vec<serde_json::Value> {
         use std::process::Command;
+        let mut result = Vec::new();
+        let bar = ProgressBar::new(jobs.len() as u64);
+        bar.set_style(
+            ProgressStyle::with_template(
+                "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}"
+            )
+                .unwrap()
+                .progress_chars("##-")
+        );
+        bar.set_message("Running FIO jobs");
         for job in jobs {
             let job_str = job.writes();
             let mut file = std::fs::File::create("temp.fio").expect("Failed to create temp file");
@@ -85,25 +116,65 @@ mod fio {
             if !output.status.success() {
                 panic!("fio command failed with status: {}", output.status);
             }
-            println!(
-                "FIO job executed successfully: {}",
-                String::from_utf8_lossy(&output.stdout)
-            );
+            let output = String::from_utf8_lossy(&output.stdout);
+            let output = if output.starts_with('{') {
+                output.to_string()
+            } else {
+                output
+                    .lines()
+                    .skip_while(|line| !line.starts_with('{'))
+                    .collect::<Vec<_>>()
+                    .join("\n")
+            };
+            let output_map: serde_json::Value = serde_json
+                ::from_str(&output)
+                .expect("Failed to parse JSON output");
+            result.push(output_map.clone());
+            log::trace!("FIO job executed successfully: {}", output_map);
+            bar.inc(1);
             // Sleep for a 5 seconds to avoid overwhelming the system
             std::thread::sleep(std::time::Duration::from_secs(5));
         }
+        bar.finish();
+        result
     }
 }
 
 fn main() {
     let args = Args::parse();
-    // println!("Testing file: {}", args.path);
-    // println!("{}", fio::is_fio_available());
-    // println!("{}", fio::get_fio_version());
-    // println!("{:?}", fio::read_config_file("./config/cdm8.fio").sections());
-    println!("{:?}", fio::gen_fio_job_configs(&fio::read_config_file("./config/cdm8.fio")));
-    println!("{:?}", fio::gen_fio_job_configs(&fio::read_config_file("./config/cdm8.fio")).len());
-    println!("{:?}", fio::run_jobs(fio::gen_fio_job_configs(&fio::read_config_file("./config/cdm8.fio"))));
+
+    let log_level = match args.log_level.as_deref() {
+        Some("trace") => log::LevelFilter::Trace,
+        Some("debug") => log::LevelFilter::Debug,
+        Some("info") => log::LevelFilter::Info,
+        Some("warn") => log::LevelFilter::Warn,
+        Some("error") => log::LevelFilter::Error,
+        _ => log::LevelFilter::Info, // Default to Info if not specified
+    };
+    INIT.call_once(|| {
+        let _ = logger::LyssaLogger::new().init(log_level);
+    });
+
+    log::trace!("Testing file: {}", args.path.unwrap());
+    let fio_available = fio::is_fio_available();
+    log::trace!("FIO available: {}", fio_available);
+    if !fio_available {
+        log::trace!("FIO is not available on this system");
+    } else {
+        log::trace!("FIO version: {}", fio::get_fio_version());
+    }
+    let fio_default_config = include_str!("../config/cdm8.fio");
+    let fio_config = fio::read_config_str(fio_default_config);
+    log::trace!("FIO config: {:?}", fio_config);
+
+    let fio_result = fio::run_jobs(fio::gen_fio_job_configs(&fio_config));
+    log::debug!("{:?}", fio_result);
+    log::debug!(
+        "{:?}",
+        fio_result[0]
+            .get("fio version")
+            .unwrap_or(&serde_json::Value::String("unknown".to_string()))
+    );
 }
 
 #[cfg(test)]
